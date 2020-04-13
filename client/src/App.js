7aa49f7c60f58c2fdaa3bf6aa7e1d8c0b4b3e709
Nick Shaw <nickwshaw@hotmail.com>
2020-04-13 13:49:10 +0100

    Changed header

--- client/src/App.js
+++ client/src/App.js
@@ -13,7 +13,7 @@ class App extends Component {
         <div className="App">
           <header className="App-header">
             <img src={logo} className="App-logo" alt="logo" />
-            <h1>Welcome</h1>
+            <h1>Fib Calculator</h1>
           </header>
           <Link to="/">Home</Link> | 
           <Link to="/otherpage">Other Page</Link> 
